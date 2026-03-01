575de38cd502cdaf9b9a499934081ef6c3639567
SilkyKitsune <wavemusictv@gmail.com>
2026-02-28 22:19:05 -0600

    Renamed KeepBoth to None, Split Combine into CombineUnder/CombineOver, and added summaries

--- MergeMode.cs
+++ MergeMode.cs
@@ -2,8 +2,18 @@
 
 public enum MergeMode
 {
+    /// <summary> Adds the patch without performing any merging </summary>
+    None,
+
+    /// <summary> Will not add if an overlapping patch is found </summary>
     Ignore,
+
+    /// <summary> Replaces the first overlapping patch found </summary>
     Replace,
-    KeepBoth,
-    Combine
+
+    /// <summary> Combines with the first overlapping patch found, prioritizing previous patch data </summary>
+    CombineUnder,
+
+    /// <summary> Combines with the first overlapping patch found, prioritizing new patch data </summary>
+    CombineOver
 }
